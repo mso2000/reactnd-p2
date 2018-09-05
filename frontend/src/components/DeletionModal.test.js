eac63b8c351242c8b7c66b3de5312ac375b4a316
Márcio Oliveira <emaildomso@gmail.com>
2018-09-05 18:12:22 -0300

    test: testes adicionados para o DeletionModal

---
+++ frontend/src/components/DeletionModal.test.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { mount } from 'enzyme'
+import DeletionModal from './DeletionModal'
+import { Button } from 'semantic-ui-react'
+
+
+describe('[Component] DeletionModal', () => {
+  let container
+
+  const setup = {
+    onCancel: jest.fn(),
+    onConfirm: jest.fn(),
+    modalBody: 'teste',
+    modalOpen: true
+  }
+
+  const wrapper = mount(<DeletionModal {...setup} />)
+
+  describe('render modal body', () => {
+    beforeEach(() => {
+      container = wrapper.find('h4')
+    })
+
+    it('should have <h4>', () => {
+      expect(container).toHaveLength(1)
+    })
+
+    it('should have <h4> with proper text', () => {
+      expect(container.text()).toEqual('teste')
+    })
+  })
+
+  describe('render modal buttons', () => {
+    beforeEach(() => {
+      container = wrapper.find('Button')
+    })
+
+    it('should have 2 Buttons', () => {
+      expect(container).toHaveLength(2)
+    })
+
+    it('call onCancel on red button click', () => {
+      container.at(0).simulate('click')
+      expect(setup.onCancel).toHaveBeenCalled()
+    })
+
+    it('call onConfirm on green button click', () => {
+      container.at(1).simulate('click')
+      expect(setup.onConfirm).toHaveBeenCalled()
+    })
+  })
+})
