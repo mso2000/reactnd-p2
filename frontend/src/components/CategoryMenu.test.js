11c9ef85d7d42c6d7aaff3175f2b2668ab029f96
Márcio <emaildomso@gmail.com>
2018-09-12 00:13:17 -0300

    test: Testes completos para CategoryMenu

--- frontend/src/components/CategoryMenu.test.js
+++ frontend/src/components/CategoryMenu.test.js
@@ -1,37 +1,46 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 import { CategoryMenu } from './CategoryMenu'
-import { MenuItem } from 'semantic-ui-react'
 import { categories } from '../utils/mockStore.js'
 
 describe('[Component] CategoryMenu', () => {
-  let wrapper, container
+  let wrapper
 
   const setup = {
     categories: categories,
     fetchData: jest.fn()
   }
 
+  const categoriesWithAll = [ {path: 'all'}, ...categories ]
+
   describe('render categories menu', () => {
-    beforeEach(() => {
-      wrapper = shallow(<CategoryMenu match={{url: '/'}} {...setup} />)
-      container = wrapper.find('MenuItem')
-    })
-
-    it(`should have "all" item`, () => {
-      expect(container.at(0).props().name).toEqual('all')
-    })
-
     it(`should have categories items`, () => {
-      categories.map(c => {
-        expect(wrapper.find(`[name="${c.path}"]`)).toHaveLength(1)
+      wrapper = shallow(<CategoryMenu match={{url: '/'}} {...setup} />)
+      categoriesWithAll.map(c => {
+        expect(wrapper.exists(`[name="${c.path}"]`)).toEqual(true)
       })
     })
 
-    it('should set "newPostModalOpen" state to true on button click', () => {
-      console.log(wrapper.find(`[name="react"]`).debug())
+    it('should match url and activate proper item only', () => {
+      categoriesWithAll.map(c => {
+        wrapper = shallow(<CategoryMenu match={{url: `/${c.path === 'all' ? '' : c.path}`}} {...setup} />)
+        expect(wrapper.find(`[name="${c.path}"]`)).toHaveLength(1)
+        expect(wrapper.find(`[name="${c.path}"]`).props().active).toEqual(true)
+      })
     })
 
-    // TODO: Testar match e testar onClick
+    it('should unmatch invalid url and activate nothing', () => {
+      wrapper = shallow(<CategoryMenu match={{url: '/invalid'}} {...setup} />)
+      expect(wrapper.exists(`[active=true]`)).toEqual(false)
+    })
+
+    it('should call fetchData on each item click', () => {
+      wrapper = shallow(<CategoryMenu match={{url: '/'}} {...setup} />)
+      categoriesWithAll.map(c => {
+        setup.fetchData.mockClear()
+        wrapper.find(`[name="${c.path}"]`).simulate('click')
+        expect(setup.fetchData).toHaveBeenCalled()
+      })
+    })
   })
 })
