8cdb6a6ef65191193bc40065f90db5711508bb3e
Márcio <emaildomso@gmail.com>
2018-09-12 00:22:52 -0300

    test: Testes adicionados para 'helpers'

---
+++ frontend/src/utils/helpers.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { formatData } from '../utils/helpers.js'
+
+describe('[Function] formatData', () => {
+  it('timestamp should be proper formatted date', () => {
+    expect(formatData(1234567890000)).toEqual('02/13/2009, 9:31:30 PM')
+  })
+})
